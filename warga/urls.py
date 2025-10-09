8f828f11400b193a8bf7a71219180c68193916ed
AgusSurya1 <agussurya112233@gmail.com>
2025-10-09 14:31:29 +0700

    pertemuan 1&2

---
+++ warga/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import (
+    WargaListView,
+    WargaDetailView,
+    PengaduanCreateView,
+
+)
+
+urlpatterns = [
+    path('', WargaListView.as_view(), name='warga-list'),
+    path('<int:pk>/', WargaDetailView.as_view(), name='warga-detail'),
+
+    # URL pengaduan
+    path('pengaduan/tambah/<int:warga_id>/', PengaduanCreateView.as_view(), name='pengaduan-tambah'),
+]
---
+++ warga/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Warga, Pengaduan
+from .forms import PengaduanForm
+
+class WargaListView(ListView):
+    model = Warga
+    template_name = 'warga/warga_list.html'
+
+
+class WargaDetailView(DetailView):
+    model = Warga
+
+    template_name = 'warga/warga_detail.html'
+# Create your views here.
+class PengaduanCreateView(CreateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+
+    def form_valid(self, form):
+        warga_id = self.kwargs['warga_id']
+        form.instance.pelapor_id = warga_id
+        form.instance.status = 'Belum Diproses'
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('warga-detail', kwargs={'pk': self.kwargs['warga_id']})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['warga_id'] = self.kwargs['warga_id']
+        return context
